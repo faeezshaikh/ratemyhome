3ba2ee7919032f41c2bd49d95298a6f83eacf289
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-01-30 14:44:59 -0600

    My contributions

--- src/pages/mycontributions/mycontributions.ts
+++ src/pages/mycontributions/mycontributions.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FirebaseProvider } from './../../providers/firebase/firebase';
+import { FirebaseListObservable } from 'angularfire2/database-deprecated';
 
 /**
  * Generated class for the MycontributionsPage page.
@@ -14,8 +16,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'mycontributions.html',
 })
 export class MycontributionsPage {
+  mycontributions:FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,  public firebaseProvider: FirebaseProvider) {
+    this.mycontributions = this.firebaseProvider.getMyContributions(1);
   }
 
   ionViewDidLoad() {
--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -15,6 +15,10 @@ export class FirebaseProvider {
  getIcoList() {
   return this.afd.list('/icoList/');
 }
+getMyContributions(id:number) {
+  id = 1;
+  return this.afd.list('/contributions/' + id + '/');
+}
 
 getIcoDetails(id:number) {
   console.log('Fetching Details for: ', id);
