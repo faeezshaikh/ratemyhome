f635c7dd6c4554779925d8b2a1e7753b9930aabd
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-02-10 14:48:45 -0600

    cleanup

--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -6,38 +6,31 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 @Injectable()
 export class FirebaseProvider {
 
- constructor(public afd: AngularFireDatabase) { }
+  constructor(public afd: AngularFireDatabase) { }
+  getIcoList() {
+    return this.afd.list('/mealList/');
+  }
+  getMyContributions(id: number) {
+    id = 1;
+    return this.afd.list('/contributions/' + id + '/');
+  }
 
- getShoppingItems() {
-   return this.afd.list('/shoppingItems/');
- }
+  getIcoDetails(id: number) {
+    console.log('Fetching Details for: ', id);
+    return this.afd.object('/mealDetails/' + id + '/');
+  }
 
- getIcoList() {
-  return this.afd.list('/mealList/');
-}
-getMyContributions(id:number) {
-  id = 1;
-  return this.afd.list('/contributions/' + id + '/');
-}
-
-getIcoDetails(id:number) {
-  console.log('Fetching Details for: ', id);
-  return this.afd.object('/mealDetails/' + id + '/');
-
-  // return this.afd.object('/icoDetails/' + id + '/').valueChanges();
-}
-
-updateIcodetails(icoDetailsId: any, status: string): void {
-  this.afd.object('/icoDetails/' + icoDetailsId)
-    .update({ status: status});    
+  updateIcodetails(icoDetailsId: any, status: string): void {
+    this.afd.object('/icoDetails/' + icoDetailsId)
+      .update({ status: status });
     console.log('Update successful');
-    
-}
- addItem(name) {
-   this.afd.list('/shoppingItems/').push(name);
- }
 
- removeItem(id) {
-   this.afd.list('/shoppingItems/').remove(id);
- }
+  }
+  addItem(name) {
+    this.afd.list('/shoppingItems/').push(name);
+  }
+
+  removeItem(id) {
+    this.afd.list('/shoppingItems/').remove(id);
+  }
 }
