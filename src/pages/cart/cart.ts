28780e06f8bba20d774b00e001951be8fc221f0c
Faeez Shaikh <faeez.shaikh@gmail.com>
2018-02-19 15:10:31 -0600

    Cart items Qty set

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -16,36 +16,44 @@ export class CartPage {
   orderId: any;
   cartItems: any;
   readyToCheckout: boolean = false;
+
+  noOf4ozItems:number = 0;
+  noOf8ozItems:number = 0;
+  product4oz:number=1;
+  product8oz:number=1;
+  totalAmount:number;
   constructor(public navCtrl: NavController, public navParams: NavParams,private firebaseProvider: FirebaseProvider) {
     // this.order = navParams.get('order');
     this.orderId = navParams.get('orderId');
     this.order = this.firebaseProvider.getOrder(this.orderId).subscribe(res => {
       this.order = res;
-      // console.log('Got order..', this.order);
-      // console.log('Got orderId..', this.orderId);
-  
-      // console.log('Max allowed:',this.order.maxallowed );
-      // console.log('Total:',this.order.totalMeals);
       
       if(this.order.maxallowed == this.order.totalMeals) {
         this.readyToCheckout = true;
-        // console.log("Flag:" ,this.readyToCheckout);
-        
       }
     });
 
   
 
      this.firebaseProvider.getCartItems(this.orderId).subscribe(res => {
-      // console.log("Results ===>" ,res);
       this.cartItems = res;
-
       this.firebaseProvider.getCartItemsBreakfast(this.orderId).subscribe(results => {
         results.forEach(item => {
           item.bfast = true;
           this.cartItems.push(item);
-        })
-        // console.log('Final Cart items', this.cartItems);
+        });
+        this.cartItems.forEach(cItem => {
+          // console.log('CartItem 4oz:',cItem.count4oz);
+          // console.log('CartItem 8oz:',cItem.count8oz);
+          
+          if(cItem.count4oz > 0) this.noOf4ozItems = this.noOf4ozItems + cItem.count4oz;
+          if(cItem.count8oz > 0) this.noOf8ozItems = this.noOf8ozItems + cItem.count8oz;
+        });
+        // console.log('4 oz items x', this.noOf4ozItems);
+        // console.log('8 oz items x', this.noOf8ozItems);
+        this.product4oz = this.noOf4ozItems*9;  // Rates set by business.
+        this.product8oz = this.noOf8ozItems*12;
+        this.totalAmount = this.product4oz + this.product8oz;
         
       });
     });
